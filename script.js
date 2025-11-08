56dbf69274b5115a3faefcd29404383b342b78fb
John Benedict A. Sales <benedictsales17@gmail.com>
2025-11-08 22:02:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const aboutBtn = document.getElementById("aboutBtn");
+  aboutBtn.addEventListener("click", function() {
+    window.location.href = "about.html";
+  });
+});
